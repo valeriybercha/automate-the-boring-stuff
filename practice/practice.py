62e3e9850d19053fd71baa382bce3e7205586c2e
Valeriy Bercha <valber.motorsport@gmail.com>
2021-03-13 22:25:59 +0200

    added new practice solutions

--- practice/practice.py
+++ practice/practice.py
@@ -98,4 +98,15 @@ def tic_tac_toe_board():
         else:
             print("Input space could not be found")    
 
-tic_tac_toe_board()
+
+
+# Manipulating strings
+
+# Picnic Table
+
+def printPicnic(itemDict, leftWidth, rightWidth):
+    print("PICNIC ITEMS".center(leftWidth + rightWidth, "-"))
+    for k, v in itemDict.items():
+        print(k.ljust(leftWidth, ".") + str(v).rjust(rightWidth))
+
+picnicItems = {"sandwiches": 4, "apples": 12, "cups": 4, "cookies": 8000}
